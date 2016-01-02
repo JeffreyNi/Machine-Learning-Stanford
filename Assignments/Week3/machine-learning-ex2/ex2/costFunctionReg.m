3702f8714e5d9b924c1bd0bfa1e3907037e84b0d
njf19920206@163.com <nif19920206@163.com>
2016-01-01 21:18:23 -0800

    01/01/2016

--- Assignments/Week3/machine-learning-ex2/ex2/costFunctionReg.m
+++ Assignments/Week3/machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,9 +17,13 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h = sigmoid(X*theta);
+J = (-1 / m )*((y')*log(h) + (1-y)'*log(1 - h));
+J = J + (lambda / (2*m))*(sum(theta.^2)-theta(1)^2);
 
-
-
+grad = (1/m)*(X'*(h-y));
+n = length(theta);
+grad = grad + (lambda/m)*([0; theta(2:n, :)]);
 
 
 % =============================================================
